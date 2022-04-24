bef8a8540e81984f61211357a260db5a439b74c5
Popov72 <github@evpopov.com>
2022-04-24 17:46:04 +0200

    Add some GUI

--- src/scenes/fluidSimulation2.ts
+++ src/scenes/fluidSimulation2.ts
@@ -1,11 +1,13 @@
 import { CreateSceneClass } from "../createScene";
 
 import * as BABYLON from "@babylonjs/core";
+import * as GUI from "@babylonjs/gui";
 
 import "./FluidRenderer/fluidRendererSceneComponent";
 import { FluidRendererGUI } from "./FluidRenderer/fluidRendererGUI";
 import { FluidSimulator, IFluidParticle } from "./FluidSimulator2/fluidSimulator";
 import { FluidRenderingObjectVertexBuffer } from "./FluidRenderer/fluidRenderingObjectVertexBuffer";
+import { IFluidRenderingRenderObject } from "./FluidRenderer/fluidRenderer";
 
 const cameraMin = 0.1;
 const cameraMax = 100;
@@ -74,7 +76,7 @@ export class FluidRendering implements CreateSceneClass {
 
         scene.createDefaultSkybox(scene.environmentTexture);
 
-        const camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, Math.PI/2.4, 30/20, new BABYLON.Vector3(0, 0, 0), scene);
+        const camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 0, Math.PI/3.0, 30/20, new BABYLON.Vector3(0, 0, 0), scene);
         camera.fov = 60 * Math.PI/180;
         camera.attachControl(canvas, true);
         camera.minZ = cameraMin;
@@ -96,86 +98,128 @@ export class FluidRendering implements CreateSceneClass {
             const numX = 10, numY = 10, numZ = 10 * 5;
 
             const numParticles = numX * numY * numZ;
-            const positions = new Float32Array(numParticles * 3);
-
-            const particles: IFluidParticle[] = [];
-            const particleRadius = 0.02;
-            let idx = 0;
-            for (let z = 0; z < numZ; ++z) {
-                for (let y = 0; y < numY; ++y) {
-                    for (let x = 0; x < numX; ++x) {
-                        positions[idx * 3 + 0] = (x - numX / 2) * particleRadius * 2;
-                        positions[idx * 3 + 1] = (y - numY / 2) * particleRadius * 2 + 0.5;
-                        positions[idx * 3 + 2] = 0.49;//z * particleRadius * 2;
-                        idx++;
-                        particles.push({
-                            density: 0,
-                            pressure: 0,
-                            accelX: 0,
-                            accelY: 0,
-                            accelZ: 0,
-                            velocityX: (Math.random() - 0.5) * 0.03,
-                            velocityY: (Math.random() - 0.5) * 0.03,
-                            velocityZ: (Math.random() - 1.0) * 0.03 - 1.4,
-                            mass: 1,
-                        });
-                    }
-                }
-            }
 
             let currNumParticles = 0;
-
-            const fluidSim = new FluidSimulator(particles, engine, positions);
-
-            fluidSim.smoothingRadius = particleRadius * 2;
-            fluidSim.currentNumParticles = currNumParticles;
-
-            (window as any).fsim = fluidSim;
-
-            const entity = fluidRenderer?.addVertexBuffer({ position: fluidSim.positionVertexBuffer }, currNumParticles, false);
-            const fluidObject = entity?.object as FluidRenderingObjectVertexBuffer;
-
-            if (entity) {
-                entity.targetRenderer.enableBlur = true;
-                entity.targetRenderer.blurFilterSize = 10;
-                entity.targetRenderer.blurDepthScale = 5;
-                //entity.targetRenderer.fluidColor = new BABYLON.Color3(0.011126082368383245*5*3, 0.05637409755197975*5*3, 1);
-                entity.targetRenderer.fluidColor = new BABYLON.Color3(1 - 0.5, 1 - 0.2, 1 - 0.05);
-                entity.targetRenderer.density = 2;
-                entity.object.particleSize = particleRadius * 2.0 * 2;
-                entity.object.particleThicknessAlpha = particleRadius;
-            }
-
+            let fluidSim: BABYLON.Nullable<FluidSimulator> = null;
+            let fluidRenderObject: IFluidRenderingRenderObject | undefined = undefined;
+            let fluidRendererGUI: FluidRendererGUI | undefined = undefined;
+            let checkBounds = true;
             let t = 0;
 
-            (window as any).doit = () => {
-                scene.onBeforeRenderObservable.add(() => {
+            const createSimulator = () => {
+                const positions = new Float32Array(numParticles * 3);
+
+                const particles: IFluidParticle[] = [];
+                const particleRadius = 0.02;
+                let idx = 0;
+                for (let z = 0; z < numZ; ++z) {
+                    for (let y = 0; y < numY; ++y) {
+                        for (let x = 0; x < numX; ++x) {
+                            positions[idx * 3 + 0] = (x - numX / 2) * particleRadius * 2;
+                            positions[idx * 3 + 1] = (y - numY / 2) * particleRadius * 2 + 0.5;
+                            positions[idx * 3 + 2] = 0.49;//z * particleRadius * 2;
+                            idx++;
+                            particles.push({
+                                density: 0,
+                                pressure: 0,
+                                accelX: 0,
+                                accelY: 0,
+                                accelZ: 0,
+                                velocityX: (Math.random() - 0.5) * 0.03,
+                                velocityY: (Math.random() - 0.5) * 0.03,
+                                velocityZ: (Math.random() - 1.0) * 0.03 - 1.4,
+                                mass: 1,
+                            });
+                        }
+                    }
+                }
+
+                currNumParticles = 0;
+                t = 0;
+
+                fluidRendererGUI?.dispose();
+
+                if (fluidRenderObject) {
+                    fluidRenderer?.removeRenderObject(fluidRenderObject);
+                }
+
+                fluidSim?.dispose();
+
+                fluidSim = new FluidSimulator(particles, engine, positions);
+
+                fluidSim.smoothingRadius = particleRadius * 2;
+                fluidSim.currentNumParticles = currNumParticles;
+                fluidSim.checkXZBounds = checkBounds;
+
+                (window as any).fsim = fluidSim;
+
+                fluidRenderObject = fluidRenderer?.addVertexBuffer({ position: fluidSim.positionVertexBuffer }, currNumParticles, false);
+
+                if (fluidRenderObject) {
+                    fluidRenderObject.targetRenderer.enableBlur = true;
+                    fluidRenderObject.targetRenderer.blurFilterSize = 10;
+                    fluidRenderObject.targetRenderer.blurDepthScale = 5;
+                    //fluidRenderObject.targetRenderer.fluidColor = new BABYLON.Color3(0.011126082368383245*5*3, 0.05637409755197975*5*3, 1);
+                    fluidRenderObject.targetRenderer.fluidColor = new BABYLON.Color3(1 - 0.5, 1 - 0.2, 1 - 0.05);
+                    fluidRenderObject.targetRenderer.density = 2;
+                    fluidRenderObject.targetRenderer.specularPower = 200;
+                    fluidRenderObject.object.particleSize = particleRadius * 2.0 * 2;
+                    fluidRenderObject.object.particleThicknessAlpha = particleRadius;
+                }
+
+                fluidRendererGUI = new FluidRendererGUI(this._scene, false);
+            }
+
+            createSimulator();
+
+            scene.onBeforeRenderObservable.add(() => {
+                if (fluidSim && fluidRenderObject) {
                     if (currNumParticles < numParticles && (t++ % 8) === 0) {
                         currNumParticles += 100;
                     }
                     fluidSim.currentNumParticles = currNumParticles;
-                    fluidObject.setNumParticles(currNumParticles);
+                    (fluidRenderObject.object as FluidRenderingObjectVertexBuffer).setNumParticles(currNumParticles);
+                
                     const numIter = 1;
                     const delta = 3.5 / 1000;
                     for (let i = 0; i < numIter; ++i) {
                         fluidSim.update(delta);
                     }
-                });
-            };
+                }
+            });
 
-            (window as any).doit2 = () => {
-                scene.onBeforeRenderObservable.add(() => {
-                    if (currNumParticles < numParticles && (t++ % 8) === 0) {
-                        currNumParticles += 100;
-                    }
-                    fluidSim.currentNumParticles = currNumParticles;
-                    fluidObject.setNumParticles(currNumParticles);
-                });
-            };
+            const advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-            new FluidRendererGUI(this._scene, false);
+            const panel = new GUI.StackPanel();
+            panel.width = "200px";
+            panel.isVertical = true;
+            panel.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+            panel.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+            advancedTexture.addControl(panel);
+        
+            const btnRestart = GUI.Button.CreateSimpleButton("btnRestart", "Restart");
+            btnRestart.width = "150px"
+            btnRestart.height = "40px";
+            btnRestart.color = "white";
+            btnRestart.cornerRadius = 20;
+            btnRestart.background = "green";
+            btnRestart.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+            btnRestart.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+            btnRestart.onPointerUpObservable.add(function() {
+                createSimulator();
+            });
+            panel.addControl(btnRestart);
 
-            scene.activeCamera = camera;
+            const stkCheckBounds = GUI.Checkbox.AddCheckBoxWithHeader("Check bounds", (v) => {
+                checkBounds = v;
+                if (fluidSim) {
+                    fluidSim.checkXZBounds = v;
+                }
+            });
+            stkCheckBounds.horizontalAlignment = GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+            stkCheckBounds.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_TOP;
+
+            panel.addControl(stkCheckBounds);
         }
 
         return scene;
